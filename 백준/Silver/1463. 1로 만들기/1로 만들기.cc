22b2865b5ca153739f575fbdd7229800d70d3bf4
juuuuung <106417758+juuuuung@users.noreply.github.com>
2023-11-03 23:28:11 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,24 +1,19 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-void init(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
-}
 
-int d[1000001] = {0};
-int n;
-
-int main(){
-  init();
-  cin >> n;
-  d[1] = 0;
-  for(int i = 2; i <= n; ++i){
-    d[i] = d[i-1]+1;
-    if(i%2==0)d[i] = min(d[i], d[i/2]+1);
-    if(i%3==0)d[i] = min(d[i], d[i/3]+1);
+int main(void){
+  int dp[1'000'002] = {0,};
+  dp[2] = 1, dp[3] = 1;
+  for(int i = 4; i <= 1'000'001; ++i){
+    int minCnt = 2'000'000'001;
+    if(i%2 == 0 && (dp[i/2] + 1) < minCnt) minCnt = dp[i/2]+1;
+    if(i%3 == 0 && (dp[i/3] + 1) < minCnt) minCnt = dp[i/3]+1;
+    if((dp[i-1] + 1) < minCnt) minCnt = dp[i-1]+1;
+    dp[i] = minCnt;
   }
-  cout << d[n];
+  int num;
+  cin >> num;
+  cout << dp[num];
   return 0;
 }
